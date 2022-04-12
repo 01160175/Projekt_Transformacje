f48b948cd53fea316c9e9f12b69af4e65e8f2438
01160175 <01160175@pw.edu.pl>
2022-04-12 19:45:18 +0200

    test

--- projekt1.py
+++ projekt1.py
@@ -13,19 +13,7 @@ class Transformacje:
     
     def __init__(self, model:str = "wgs84"):
         
-        if model == "wgs84":
-            self.a = 6378137.0
-            self.b = 6356752.31424518
-            
-        elif model == "grs80":
-            self.a = 6378137.0
-            self.b = 6356752.31414036
-            
-        else:
-            raise NotImplementedError(f"{model} model not implemented")
-        
-        self.flattening = (self.a-self.b)/ self.a
-        self.ecc = 2*self.flattening - self.flattening**2
+
 
     def xyz_2_blh(X, Y, Z, self):
         """
